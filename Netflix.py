9321b4b9591c526f1a29667966cd9ff0f1c7931a
Alex Fortin <alexfortin@utexas.edu>
2015-02-14 14:52:30 -0600

    Initial skeleton of implementation closes #18

--- Netflix.py
+++ Netflix.py
@@ -7,35 +7,75 @@
 from functools import reduce
 from numpy     import mean, sqrt, square, subtract
 from sys       import version
+import json
 
+predictedData = []
+actualData = []
 
-def rmse_numpy (a, p) :
-   return sqrt(mean(square(subtract(a, p))))
+def rmse(predictedData, actualData) :
+    assert(hasattr(predictedData, "__iter__"))
+    assert(hasattr(actualData, "__iter__"))
+
+    return sqrt(mean(square(subtract(predictedData, actualData))))
 
 def netflix_solve(r, w) :
     """
     r a reader
     w a writer
     """
-    _list = []
+    global actualData
+    global predictedData
+
+    # w.write(str(rmse(predictedData, actualData)))
+
+    customerList = []
     movieID = ""
     for s in r :
         if ':' in s:
-            # do some work
+            assert(':' in s)
+
             if movieID == "" :
                 movieID = s
             else :
-                # process movieID with _list
-                # set movieID to s
-                netflix_write(movieID, _list, w)
+                assert(len(movieID) > 0)
+                
+                customerRating = predictRatings(movieID, customerList)
+                actualRating(movieID, customerList)
+                netflix_write(movieID, customerRating, w)
+                del customerList [:]
+                movieID = s
         else :
-            _list.append(s)
-            
+            customerList.append(s)
+    customerRating = predictRatings(movieID, customerList)
+    actualRating(movieID, customerList)
+    netflix_write(movieID, customerRating, w)
+
+    w.write(str(rmse(predictedData, actualData)) + "\n")
         
             
-def netflix_write(movieID, customers, w) :
+def netflix_write(movieID, customerRatings, w) :
+    assert(len(movieID) > 0)
+
     w.write(movieID)
-    for s in customers :
-        w.write(s)
+    for s in customerRatings :
+        w.write(s + "\n")
 
 
+def predictRatings(movieID, customerList) :
+    assert(len(movieID) > 0)
+
+    customerRating = []
+    global predictedData
+    for s in customerList :
+        customerRating.append("3.7")
+        predictedData.append(3.7) 
+    return customerRating    
+
+def actualRating(movieID, customerList) :
+    assert(len(movieID) > 0)
+    assert(len(customerList) > 0)
+
+    global actualData
+    actualFile = json.load(open('/u/afortin/downing/cs373/p2-netflix/netflix-tests/pma459-answersCache.json'))
+    for s in customerList :
+        actualData.append(int(actualFile[movieID[:-2]][s[:-1]]))

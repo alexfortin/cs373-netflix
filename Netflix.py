d59b3a5502337e6456dc8e2a33a603572ff75848
Aaron <aaron.dishman@utexas.edu>
2015-02-14 12:06:59 -0600

    piping tests

--- Netflix.py
+++ Netflix.py
@@ -12,3 +12,14 @@ from sys       import version
 def rmse_numpy (a, p) :
    return sqrt(mean(square(subtract(a, p))))
 
+def netflix_solve(r, w) :
+    """
+    r a reader
+    w a writer
+    """
+    for s in r :
+        movieID = s
+        v    = netflix_eval(i, j)
+        netflix_print(w, i, j, v)
+
+
